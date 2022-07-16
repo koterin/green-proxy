e240944389c7052757a42ddee778ebe4b267f834
koterin <katerine.grin@gmail.com>
2022-07-17 02:16:11 +0300

    deleted set env

--- set-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ./env/.sh-env
-
-echo export PROXY_LOCALHOST=$PROXY_LOCALHOST
-echo export PROXY_PORT=$PROXY_PORT
-echo export PUBLIC_URL=$PUBLIC_URL
-echo export AUTH_SERVER_URL=$AUTH_SERVER_URL
-echo export AUTH_API_URL=$AUTH_API_URL
